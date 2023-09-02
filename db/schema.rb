3b57e73995e842249e4f535ab255f8598f8e6853
d.s. <serrano.david.liaca@gmail.com>
2023-09-02 20:18:10 +0200

    comments for meetings ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_02_165630) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_02_174036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "meeting_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["meeting_id"], name: "index_comments_on_meeting_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.integer "floor"
@@ -58,6 +68,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_02_165630) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "meetings"
+  add_foreign_key "comments", "users"
   add_foreign_key "flats", "users"
   add_foreign_key "meetings", "syndics"
 end
