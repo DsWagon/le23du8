5c2324be8aecc28ab9a24b496e51970dc2e81e8f
d.s. <serrano.david.liaca@gmail.com>
2023-09-11 16:47:27 +0200

    js toggle pour quote

---
+++ app/javascript/controllers/quote_view_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["cardView", "tableView"]
+
+
+  toggleCardView() {
+    this.cardViewTargets.forEach((el) => {
+      el.style.display = "block"
+    })
+    this.tableViewTarget.style.display = "none"
+  }
+
+  toggleTableView() {
+    this.cardViewTargets.forEach((el) => {
+      el.style.display = "none"
+    })
+    this.tableViewTarget.style.display = "block"
+  }
+}
