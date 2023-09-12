242966b9868d8406dd2db670db88b5a81e4e11c6
d.s. <serrano.david.liaca@gmail.com>
2023-09-12 16:53:37 +0200

    button switch color

--- app/javascript/controllers/theme_toggle_controller.js
+++ app/javascript/controllers/theme_toggle_controller.js
@@ -1,10 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["btnSwitch"];
+
   toggleTheme(event) {
     const root = document.documentElement;
     const currentTheme = root.getAttribute('data-bs-theme');
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     root.setAttribute('data-bs-theme', newTheme);
+
+    // Basculer les icônes en utilisant les classes CSS
+    this.btnSwitchTargets.forEach(target => {
+      target.dataset.theme = newTheme;
+    });
   }
 }
