9c510b738fe8ff8a7abfcfcaaca4ea1cfa70b236
d.s. <serrano.david.liaca@gmail.com>
2023-09-13 17:03:21 +0200

    l18n + fr.yml

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -15,6 +14,8 @@ module Le23test
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :fr
 
     # Configuration for the application, engines, and railties goes here.
     #
