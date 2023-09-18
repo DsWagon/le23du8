2834a560570637639a0254afc38188a3d013630f
d.s. <serrano.david.liaca@gmail.com>
2023-09-18 22:21:47 +0200

    dot??

--- app/javascript/controllers/trix.js
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  document.addEventListener("trix-file-accept", (e) => {
-    e.preventDefault();
-    });
-  }
