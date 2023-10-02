24a9293d7ad4e8ef6e3d56d759b52b7cf9347ea8
d.s. <serrano.david.liaca@gmail.com>
2023-10-02 16:22:37 +0200

    simple form post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,8 @@ class Post < ApplicationRecord
   has_many :commentaries, dependent: :destroy
   has_rich_text :content
 
-  enum category: [:vente, :achat, :don, :news]
+  enum category: [:news, :vente, :achat, :don]
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
